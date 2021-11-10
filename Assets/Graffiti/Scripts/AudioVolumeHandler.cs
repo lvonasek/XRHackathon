89084b7303dc2d4fc659f8cab01c94e88a829617
Lubos Vonasek <tridosm@gmail.com>
2021-11-10 17:07:56 +0100

    Background audio added

---
+++ Assets/Graffiti/Scripts/AudioVolumeHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class AudioVolumeHandler : MonoBehaviour
+{
+    [SerializeField]
+    private float fadingSpeed = 0.03f;
+    [SerializeField]
+    private AudioSource sfx;
+
+    private void Update()
+    {
+        float targetVolume = OVRManager.hasInputFocus ? 1 : 0;
+        sfx.volume = Mathf.Lerp(sfx.volume, targetVolume, fadingSpeed);
+    }
+}
